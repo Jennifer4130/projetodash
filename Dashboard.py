6cd1976fd99cd58265d09e32bf72ac6d8a52f954
kaioryu07 <kaioh3107@gmail.com>
2026-05-04 21:39:14 -0300

    Projeto Completo

--- Dashboard.py
+++ Dashboard.py
@@ -43,7 +43,7 @@ def main():
     )
 
     custo_lucro_data = data_filtrada(['Segmento'])[['COGS', 'Lucro']].sum().reset_index().melt(
-        id_vars ='Segmento',Value_vars = ['COGS', 'Lucro'])
+        id_vars ='Segmento', value_vars = ['COGS', 'Lucro'])
     custo_lucro_data['value_formatado'] = custo_lucro_data['value'].apply(lambda x: f'R${x:.2}')
     gf_custo_lucro = px.bar(
         custo_lucro_data,
